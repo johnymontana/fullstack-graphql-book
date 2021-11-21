ac1071ce1f5123abd67963312e87f093972afd03
johnymontana <lyonwj@gmail.com>
2021-11-21 08:12:21 -0700

    update chapter 4 code

--- code/chapter4/index.js
+++ code/chapter4/index.js
@@ -1,14 +1,14 @@
 const { ApolloServer } = require("apollo-server");
 const neo4j = require("neo4j-driver");
-const { makeAugmentedSchema } = require("neo4j-graphql-js");
+const { Neo4jGraphQL } = require("@neo4j/graphql");
 
 const resolvers = {
   Business: {
     waitTime: (obj, args, context, info) => {
       var options = [0, 5, 10, 15, 30, 45];
       return options[Math.floor(Math.random() * options.length)];
-    }
-  }
+    },
+  },
 };
 
 const typeDefs = /* GraphQL */ `
@@ -24,7 +24,7 @@ const typeDefs = /* GraphQL */ `
 
   type Business {
     businessId: ID!
-    waitTime: Int! @neo4j_ignore
+    waitTime: Int! @ignore
     averageStars: Float!
       @cypher(
         statement: "MATCH (this)<-[:REVIEWS]-(r:Review) RETURN avg(r.stars)"
@@ -42,14 +42,14 @@ const typeDefs = /* GraphQL */ `
     state: String!
     address: String!
     location: Point!
-    reviews: [Review] @relation(name: "REVIEWS", direction: "IN")
-    categories: [Category] @relation(name: "IN_CATEGORY", direction: "OUT")
+    reviews: [Review] @relationship(type: "REVIEWS", direction: IN)
+    categories: [Category] @relationship(type: "IN_CATEGORY", direction: OUT)
   }
 
   type User {
     userID: ID!
     name: String!
-    reviews: [Review] @relation(name: "WROTE", direction: "OUT")
+    reviews: [Review] @relationship(type: "WROTE", direction: OUT)
   }
 
   type Review {
@@ -57,35 +57,25 @@ const typeDefs = /* GraphQL */ `
     stars: Float!
     date: Date!
     text: String
-    user: User @relation(name: "WROTE", direction: "IN")
-    business: Business @relation(name: "REVIEWS", direction: "OUT")
+    user: User @relationship(type: "WROTE", direction: IN)
+    business: Business @relationship(type: "REVIEWS", direction: OUT)
   }
 
   type Category {
     name: String!
-    businesses: [Business] @relation(name: "IN_CATEGORY", direction: "IN")
+    businesses: [Business] @relationship(type: "IN_CATEGORY", direction: IN)
   }
 `;
-
-const schema = makeAugmentedSchema({
-  typeDefs,
-  resolvers,
-  config: {
-    mutation: false,
-    query: {
-      exclude: ["MySecretType"]
-    }
-  }
-});
 
 const driver = neo4j.driver(
   "bolt://localhost:7687",
   neo4j.auth.basic("neo4j", "letmein")
 );
 
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
 const server = new ApolloServer({
-  schema,
-  context: { driver }
+  schema: neoSchema.schema,
 });
 
 server.listen().then(({ url }) => {
