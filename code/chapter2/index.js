644a73c29daae97c2245a55267e97b785529d978
johnymontana <lyonwj@gmail.com>
2022-04-07 14:10:07 -0600

    chapter 2 code update

--- code/chapter2/index.js
+++ code/chapter2/index.js
@@ -60,20 +60,20 @@ const typeDefs = /* GraphQL */ `
     name: String
     address: String
     avgStars: Float
-    photos(first: Int = 3, offset: Int = 0): [Photo]
-    reviews(first: Int = 3, offset: Int = 0): [Review]
+    photos(first: Int = 3, offset: Int = 0): [Photo!]!
+    reviews(first: Int = 3, offset: Int = 0): [Review!]!
   }
 
   type User {
     userId: ID!
     name: String
-    photos: [Photo]
-    reviews: [Review]
+    photos: [Photo!]!
+    reviews: [Review!]!
   }
 
   type Photo {
-    business: Business
-    user: User
+    business: Business!
+    user: User!
     photoId: ID!
     url: String
   }
@@ -82,8 +82,8 @@ const typeDefs = /* GraphQL */ `
     reviewId: ID!
     stars: Float
     text: String
-    user: User
-    business: Business
+    user: User!
+    business: Business!
   }
 
   enum BusinessOrdering {
@@ -92,13 +92,13 @@ const typeDefs = /* GraphQL */ `
   }
 
   type Query {
-    allBusinesses(first: Int = 10, offset: Int = 0): [Business]
+    allBusinesses(first: Int = 10, offset: Int = 0): [Business!]!
     businessBySearchTerm(
       search: String
       first: Int = 10
       offset: Int = 0
       orderBy: BusinessOrdering = name_asc
-    ): [Business]
+    ): [Business!]!
     userById(id: ID!): User
   }
 `;
