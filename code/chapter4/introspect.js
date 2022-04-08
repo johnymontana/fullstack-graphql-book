a4b4ef4b7121ffb525af97bab5c279e4afdbc895
johnymontana <lyonwj@gmail.com>
2022-04-08 14:58:06 -0600

    update ch4 code

---
+++ code/chapter4/introspect.js
@@ -0,0 +1,13 @@
+const { toGraphQLTypeDefs } = require("@neo4j/introspector");
+const neo4j = require("neo4j-driver");
+const fs = require("fs");
+const driver = neo4j.driver("neo4j://localhost:7687", neo4j.auth.basic("neo4j", "password"));
+const sessionFactory = () => driver.session({ defaultAccessMode: neo4j.session.READ });
+// We create a async function here until "top level await" has landed
+// so we can use async/await
+async function main() {
+    const typeDefs = await toGraphQLTypeDefs(sessionFactory);
+    fs.writeFileSync("schema.graphql", typeDefs);
+    await driver.close();
+}
+main();
